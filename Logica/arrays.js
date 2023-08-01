8e746bd44c83379a9c371a25112a259dd834c108
Ronaldo Carneval <ronaldo_carneval@hotmail.com>
2023-08-01 16:05:20 -0300

    commit

---
+++ Logica/arrays.js
@@ -0,0 +1,9 @@
+let nome = ["Ricardo", "Alice", "Alan", "João", "Gilberto", "Leonardo", "Joana", "Roberta", "Rino", "Enzo"];
+let idade = [30, 25, 47, 50, 27, 17, 25, 9, 10, 37];
+let cor = ["branco", "rosa", "roxo", "verde", "lilas", "preto", "vermelho", "azul", "branco", "verde"];
+console.log(nome, idade, cor);
+cor.pop();
+console.log(cor);
+cor.push("verde");
+console.log(cor);
+
