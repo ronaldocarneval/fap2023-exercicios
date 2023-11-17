a8040b5e02af4cfccc94a4e58bd0a98d75b8fdde
Ronaldo Carneval <ronaldo_carneval@hotmail.com>
2023-11-17 19:54:08 -0300

    Commit q06

---
+++ Javascript/06_exececao.js
@@ -0,0 +1,9 @@
+try {
+    let num1 = parseInt(window.prompt("Digite a sua primeira nota:"));
+let num2 = parseInt(window.prompt("Digite a sua segunda nota:"));
+let num3 = parseInt(window.prompt("Digite a sua terceira nota:"));
+media = (num1 + num2 + num3)/3;
+document.getElementById("resultado").innerHTML = media;
+} catch (num1 > 10 ) {
+    console.log("Média tem que ser menor que 10")
+}
